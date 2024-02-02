ab1b202fb33f126300fbd60e60eeb4e12c765b8b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-02 21:28:40 +0600

    finished the work for now

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const box = document.getElementById('box');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
 
 button1.addEventListener('click', () => {
   box.style.transform = 'translateX(-44px)';
@@ -8,4 +10,12 @@ button1.addEventListener('click', () => {
 
 button2.addEventListener('click', () => {
   box.style.transform = 'translateX(44px)';
+});
+
+button3.addEventListener('click', () => {
+  box.style.transform = 'translateY(-44px)';
+});
+
+button4.addEventListener('click', () => {
+  box.style.transform = 'translateY(44px)';
 });
