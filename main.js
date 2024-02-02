725eec127706acb1a175e90474cbb7a02ac98289
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-02 21:01:01 +0600

    finished the work in the styling and the main script work

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const box = document.getElementById('box');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
+
+button1.addEventListener('click', () => {
+  box.style.transform = 'translateX(-44px)';
+});
+
+button2.addEventListener('click', () => {
+  box.style.transform = 'translateX(44px)';
+});
