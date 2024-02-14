fe22f78705e3e0a8aa6d99836f40caa0cfde6e92
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-14 14:28:40 +0600

    added box movement when the keyboard arrow key is clicked

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
 const button4 = document.getElementById('button4');
 
+// adding the button arrow key function
 button1.addEventListener('click', () => {
   box.style.transform = 'translateX(-44px)';
 });
@@ -18,4 +19,42 @@ button3.addEventListener('click', () => {
 
 button4.addEventListener('click', () => {
   box.style.transform = 'translateY(44px)';
+});
+
+// adding the keyboard arrow key function
+window.addEventListener('keydown', (event) => {
+  // try with the if else statement
+  if (event.key === 'ArrowLeft') {
+    box.style.transform = 'translateX(-44px)';
+    console.log('arrow left clicked');
+  } else if (event.key === 'ArrowRight') {
+    box.style.transform = 'translateX(44px)';
+    console.log('arrow right clicked');
+  } else if (event.key === 'ArrowUp') {
+    box.style.transform = 'translateY(-44px)';
+    console.log('arrow up clicked');
+  } else if (event.key === 'ArrowDown') {
+    box.style.transform = 'translateY(44px)';
+    console.log('arrow down clicked');
+  }
+
+  // try with the switch statement
+  switch (event.key) {
+    case 'ArrowLeft':
+      box.style.transform = 'translateX(-44px)';
+      console.log('arrow left clicked');
+      break;
+    case 'ArrowRight':
+      box.style.transform = 'translateX(44px)';
+      console.log('arrow right clicked');
+      break;
+    case 'ArrowUp':
+      box.style.transform = 'translateY(-44px)';
+      console.log('arrow Up clicked');
+      break;
+    case 'ArrowDown':
+      box.style.transform = 'translateY(44px)';
+      console.log('arrow right clicked');
+      break;
+  }
 });
